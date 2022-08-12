0f4fd4ed471566019a3770f80e4cde1678016f26
Chalisa <chalisapusitdhikul@gmail.com>
2022-08-12 15:05:37 +0100

    Add temperature sensor

--- oled_screen.py
+++ oled_screen.py
@@ -7,3 +7,38 @@ display.text('rEvolver', 5, 5, 1)
 display.text('Vivo La', 5, 14, 1)
 display.text('Evolution', 5, 22, 1)
 display.show()
+
+#temperature sensor
+import machine, onewire, ds18x20
+from machine import Pin, I2C
+from time import sleep
+from ssd1306 import SSD1306_I2C
+
+i2c=I2C(0,sda=Pin(12), scl=Pin(9), freq=400000)
+display = SSD1306_I2C(128, 32, i2c)
+
+ds_pin = machine.Pin(22)
+ 
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+ 
+roms = ds_sensor.scan()
+ 
+print('Found DS devices')
+print('Temperature (°C)')
+ 
+while True:
+ 
+  ds_sensor.convert_temp()
+ 
+  sleep(1)
+ 
+  for rom in roms:
+ 
+    print(ds_sensor.read_temp(rom))
+    display.fill(0)
+    display.text("Temperature (C)", 5, 5, 1)
+    display.text(str(ds_sensor.read_temp(rom)), 5, 14, 1)
+    display.show()
+ 
+  sleep(3)
+
