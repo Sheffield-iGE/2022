eae32c2b9ebaff9b24b4901f1ccad9c63a1d0325
Chalisa <chalisapusitdhikul@gmail.com>
2022-08-16 16:20:37 +0100

    invert OD recording

--- phototransistor.py
+++ phototransistor.py
@@ -1,13 +1,13 @@
 from machine import ADC, Pin
 from time import sleep
-
+#run at 50% power by changing current to 50mA
 photoPIN = 26
 
 def readLight(photoGP):
     photoRes = ADC(Pin(26))
     light = photoRes.read_u16()
     light = round(light/65535*100,2)
-    return light
+    return 100-light
 
 while True:
     print("light: " + str(readLight(photoPIN)) +"%")
