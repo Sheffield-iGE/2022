30e39017f07edce23129f03d95b2a6c3292fab5f
Chalisa <chalisapusitdhikul@gmail.com>
2022-08-24 11:33:56 +0100

    running pico

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from time import sleep
-import combined, asyncio
+import combined, ds18x20, onewire, asyncio
 
+#servo motor
+while True:
+    servomove(-100)
+    sleep(5)
+    servomove(50)
+    sleep(3)
+    servomove(100)
+    sleep(5)
+    servomove(50)
+#heating controller
+    ds_sensor.convert_temp()
+    time.sleep_ms(750)
+    for rom in roms:
+        print(round(ds_sensor.read_temp(rom), 1))
+    time.sleep(0.3)
+    if ds_sensor.read_temp(rom) < 32:
+        motorMove(100,1,pwmPIN,cwPin,acwPin)
+        print("ON")
+    else:
+        motorMove(0,0,pwmPIN,cwPin,acwPin)
+        print("OFF")
