9c01610fa21e4b75b2d8c30f31c49cad7c15d026
Chalisa <chalisapusitdhikul@gmail.com>
2022-08-24 10:15:28 +0100

    temperature controller and heating

--- heating.py
+++ heating.py
@@ -1,15 +1,46 @@
 from machine import Pin, PWM
 from time import sleep
-import ds18b20 
+import onewire, ds18x20, time
 
-IN1 = Pin(4, Pin.OUT)
-IN2 = Pin(3, Pin.OUT)
+pwmPIN=4
+cwPin=3 
+acwPin=2
 
-speed = PWM(Pin(5))
-speed.freq(1000)
+def motorMove(speed,direction,speedGP,cwGP,acwGP):
+  if speed > 100: speed=100
+  if speed < 0: speed=0
+  Speed = PWM(Pin(speedGP))
+  Speed.freq(50)
+  cw = Pin(cwGP, Pin.OUT)
+  acw = Pin(acwGP, Pin.OUT)
+  Speed.duty_u16(int(speed/100*65536))
+  if direction < 0:
+      cw.value(0)
+      acw.value(1)
+  if direction == 0:
+      cw.value(0)
+      acw.value(0)
+  if direction > 0:
+      cw.value(1)
+      acw.value(0)
 
-while ds_sensor.read_temp(rom) < 37:
-        speed.duty_u16(10000)
-        IN1.low()  
-        IN2.high()
-        sleep(5)
+ds_pin = machine.Pin(21)
+ 
+ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
+ 
+roms = ds_sensor.scan()
+
+# main program
+while True:
+    ds_sensor.convert_temp()
+    time.sleep_ms(750)
+    for rom in roms:
+        print(round(ds_sensor.read_temp(rom), 1))
+    time.sleep(0.3)
+    if ds_sensor.read_temp(rom) < 32:
+        motorMove(100,1,pwmPIN,cwPin,acwPin)
+        print("ON")
+    else:
+        motorMove(0,0,pwmPIN,cwPin,acwPin)
+        print("OFF")
+#motorMove(100,0,pwmPIN,cwPin,acwPin)
