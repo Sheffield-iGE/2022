1a7bdfa32c9f6717401f0e4e033f5f23e23914b6
Chalisa <chalisapusitdhikul@gmail.com>
2022-08-24 10:57:01 +0100

    combined code

--- combined.py
+++ combined.py
@@ -3,35 +3,35 @@ from machine import Pin, PWM, ADC
 import onewire, ds18x20, time
 
 #servo motor
-pwm = PWM(Pin(1))
-pwm.freq(50)
+pwmservo = PWM(Pin(1))
+pwmservo.freq(50)
 # 1ms = round(65535/20)
 # 2ms = round(65535/10)
 # 1.5ms = round(65535*0.075)
 def servomove(power):
-    pwm.duty_u16(int((power/100)*1638.375+4915.125))
+    pwmservo.duty_u16(int((power/100)*1638.375+4915.125))
 
 #heating coil
-pwmPIN=4
-cwPin=3 
-acwPin=2
-def motorMove(speed,direction,speedGP,cwGP,acwGP):
-  if speed > 100: speed=100
-  if speed < 0: speed=0
-  Speed = PWM(Pin(speedGP))
-  Speed.freq(50)
-  cw = Pin(cwGP, Pin.OUT)
-  acw = Pin(acwGP, Pin.OUT)
-  Speed.duty_u16(int(speed/100*65536))
-  if direction < 0:
-      cw.value(0)
-      acw.value(1)
-  if direction == 0:
-      cw.value(0)
-      acw.value(0)
-  if direction > 0:
-      cw.value(1)
-      acw.value(0)
+pwmheat=4
+cwPinheat=3 
+acwPinheat=2
+def motorMoveheat(speedheat,directionheat,speedGPheat,cwGPheat,acwGPheat):
+  if speedheat > 100: speedheat=100
+  if speedheat < 0: speedheat=0
+  Speedheat = PWM(Pin(speedGPheat))
+  Speedheat.freq(50)
+  cwheat = Pin(cwGPheat, Pin.OUT)
+  acwheat = Pin(acwGPheat, Pin.OUT)
+  Speedheat.duty_u16(int(speedheat/100*65536))
+  if directionheat < 0:
+      cwheat.value(0)
+      acwheat.value(1)
+  if directionheat == 0:
+      cwheat.value(0)
+      acwheat.value(0)
+  if directionheat > 0:
+      cwheat.value(1)
+      acwheat.value(0)
 ds_pin = machine.Pin(21) 
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 roms = ds_sensor.scan()
@@ -45,25 +45,25 @@ def readLight(photoGP):
     return 100-light
 
 #pump , max speed = 60mL/min
-adc = ADC(Pin(27))
-pwm = PWM(Pin(16))   
-pwmPIN=16
-cwPin=14 
-acwPin=15
-def motorMove(speed,direction,speedGP,cwGP,acwGP):
-  if speed > 100: speed=100
-  if speed < 0: speed=0
-  Speed = PWM(Pin(speedGP))
-  Speed.freq(50)
-  cw = Pin(cwGP, Pin.OUT)
-  acw = Pin(acwGP, Pin.OUT)
-  Speed.duty_u16(int(speed/100*65536))
-  if direction < 0:
-      cw.value(0)
-      acw.value(1)
-  if direction == 0:
-      cw.value(0)
-      acw.value(0)
-  if direction > 0:
-      cw.value(1)
-      acw.value(0)
+adcpump = ADC(Pin(27))
+pwmpump = PWM(Pin(16))   
+pwmpump=16
+cwPinpump=14 
+acwPinpump=15
+def motorMovepump(speedpump,directionpump,speedGPpump,cwGPpump,acwGPpump):
+  if speedpump > 100: speedpump=100
+  if speedpump < 0: speedpump=0
+  Speedpump = PWM(Pin(speedGPpump))
+  Speedpump.freq(50)
+  cwpump = Pin(cwGPpump, Pin.OUT)
+  acwpump = Pin(acwGPpump, Pin.OUT)
+  Speedpump.duty_u16(int(speedpump/100*65536))
+  if directionpump < 0:
+      cwpump.value(0)
+      acwpump.value(1)
+  if directionpump == 0:
+      cwpump.value(0)
+      acwpump.value(0)
+  if directionpump > 0:
+      cwpump.value(1)
+      acwpump.value(0)
