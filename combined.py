01e0d12bd2516774ede0c1eac9bfe5d8b5ed100c
Chalisa <chalisapusitdhikul@gmail.com>
2022-08-24 10:39:33 +0100

    combined code

--- combined.py
+++ combined.py
@@ -44,22 +44,26 @@ def readLight(photoGP):
     light = round(light/65535*100,2)
     return 100-light
 
-while True:
-    ds_sensor.convert_temp() #heating
-    time.sleep_ms(750)
-    for rom in roms:
-        print(round(ds_sensor.read_temp(rom), 1))
-        time.sleep(0.3)
-    if ds_sensor.read_temp(rom) < 32:
-        motorMove(100,1,pwmPIN,cwPin,acwPin)
-        print("ON")
-    else:
-        motorMove(0,0,pwmPIN,cwPin,acwPin)
-        print("OFF")
-    servomove(-100)  #heating
-    sleep(5)
-    servomove(50)
-    sleep(3)
-    servomove(100)
-    sleep(5)
-    servomove(50)
+#pump , max speed = 60mL/min
+adc = ADC(Pin(27))
+pwm = PWM(Pin(16))   
+pwmPIN=16
+cwPin=14 
+acwPin=15
+def motorMove(speed,direction,speedGP,cwGP,acwGP):
+  if speed > 100: speed=100
+  if speed < 0: speed=0
+  Speed = PWM(Pin(speedGP))
+  Speed.freq(50)
+  cw = Pin(cwGP, Pin.OUT)
+  acw = Pin(acwGP, Pin.OUT)
+  Speed.duty_u16(int(speed/100*65536))
+  if direction < 0:
+      cw.value(0)
+      acw.value(1)
+  if direction == 0:
+      cw.value(0)
+      acw.value(0)
+  if direction > 0:
+      cw.value(1)
+      acw.value(0)
