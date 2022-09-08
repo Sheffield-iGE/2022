6403778a0481e199199e17fb320f6d85d564772a
Chalisa <chalisapusitdhikul@gmail.com>
2022-09-08 15:08:42 +0100

    Complete

--- Bioreactor/main.py
+++ Bioreactor/main.py
@@ -1,10 +1,21 @@
-from time import sleep, time
+from time import sleep, time_ns, time, ticks_us, ticks_diff
 from drivers import *
 
-stirringtimer = time()
+stirringtimer = ticks_us()
+pidtimer = ticks_us()
 i1 = readintensity()
+esum = 0
 
 while True:
+    target_temp = 37
+    p = 10
+    i = 0.1
+    error = target_temp - readTemp()
+    pidtime = ticks_diff(ticks_us(),pidtimer)/1000000
+    esum += error * pidtime
+    print(error, esum)
+    pidtimer = ticks_us()
+    heatingPower((p * error + i * esum) * 10)
     now = time()
     if now-stirringtimer < 5:
         servomove(-100) 
@@ -16,22 +27,15 @@ while True:
         servomove(0)
     else:
         stirringtimer = time()
-        print(readTemp())
+        print(readTemp(), p + error + i * esum)
 #     print(now-stirringtimer)
     i2 = readintensity()
     display.fill(0)
     display.text("light: " + str(readOD(i1, i2)), 5, 5, 1)
     display.text("temp : " + str(round(readTemp(), 1)) + "C", 5, 14, 1)
     display.show()
-    sleep(0.3)
-    
-    esum = 0
-    target_temp = 37
-    p = 1
-    i = 0.01
-    error = target_temp - readTemp()
-    esum += error
-    heatingPower((p * error + i * esum) * 10)
+#     sleep(0.3)
+
 #     print(p + error + i * esum)
 # 
 #     if readTemp() < 37:
@@ -62,4 +66,4 @@ while True:
 #         print(adcpump.read_u16())
     else:
         pumpPower(0)
-    sleep(0.1) 
+    sleep(0.4) 
