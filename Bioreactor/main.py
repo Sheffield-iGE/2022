219e89f0e39d81ace79f19748e1398dbb19eafbc
Chalisa <chalisapusitdhikul@gmail.com>
2022-09-08 13:37:01 +0100

    Complete

--- Bioreactor/main.py
+++ Bioreactor/main.py
@@ -3,20 +3,20 @@ from drivers import *
 
 stirringtimer = time()
 i1 = readintensity()
-print(i1)
 
 while True:
-#     now = time()
-#     if now-stirringtimer < 5:
-#         servomove(-100) 
-#     elif now-stirringtimer < 8:
-#         servomove(0)
-#     elif now-stirringtimer < 13:
-#         servomove(100)
-#     elif now-stirringtimer < 16:
-#         servomove(0)
-#     else:
-#         stirringtimer = time()
+    now = time()
+    if now-stirringtimer < 5:
+        servomove(-100) 
+    elif now-stirringtimer < 8:
+        servomove(0)
+    elif now-stirringtimer < 13:
+        servomove(100)
+    elif now-stirringtimer < 16:
+        servomove(0)
+    else:
+        stirringtimer = time()
+        print(readTemp())
 #     print(now-stirringtimer)
     i2 = readintensity()
     display.fill(0)
@@ -25,43 +25,41 @@ while True:
     display.show()
     sleep(0.3)
     
-#     esum = 0
-#     target_temp = 37
-#     p = 1
-#     i = 0.01
-#     error = target_temp - readTemp()
-#     esum += error
-#     heatingPower((p * error + i * esum) * 10)
+    esum = 0
+    target_temp = 37
+    p = 1
+    i = 0.01
+    error = target_temp - readTemp()
+    esum += error
+    heatingPower((p * error + i * esum) * 10)
 #     print(p + error + i * esum)
 # 
-    if readTemp() < 36.8:
-        heatingPower(100)
-    elif readTemp() < 37:
-        esum = 0
-        target_temp = 37
-        p = 1
-        i = 0.01
-        error = target_temp - readTemp()
-        esum += error
-        heatingPower((p * error + i * esum) * 10)
-        print(p + error + i * esum)
-    elif readTemp() > 38:
-        heatingPower(0)
-    else:
-        esum = 0
-        target_temp = 37
-        p = 1
-        i = 0.01
-        error = target_temp - readTemp()
-        esum += error
-        heatingPower((p * error + i * esum) * 100)
-        print((p * error + i * esum) * 100)
+#     if readTemp() < 37:
+#         heatingPower(100)
+#     elif readTemp() < 37:
+#         esum = 0
+#         target_temp = 37
+#         p = 1
+#         i = 0.01
+#         error = target_temp - readTemp()
+#         esum += error
+#         heatingPower((p * error + i * esum) * 10)
+#         print(p + error + i * esum)
+#     elif readTemp() > 37:
+#         heatingPower(0)
+#     else:
+#         esum = 0
+#         target_temp = 37
+#         p = 1
+#         i = 0.01
+#         error = target_temp - readTemp()
+#         esum += error
+#         heatingPower((p * error + i * esum) * 100)
+#         print((p * error + i * esum) * 100)
     if readOD(i1, i2) > 0.5:
         speed = adcpump.read_u16()
         pumpPower(speed/65535*100)
-        print(adcpump.read_u16())
+#         print(adcpump.read_u16())
     else:
         pumpPower(0)
-    print(i1)
-    print(i2)
     sleep(0.1) 
