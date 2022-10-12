199611dd5dc3a5dae835bfe7f7b5f1dc584237a3
Brooks J Rady <b.j.rady@gmail.com>
2022-10-12 05:41:14 +0100

    Final bioreactor code

--- Bioreactor/drivers.py
+++ Bioreactor/drivers.py
@@ -1,21 +1,33 @@
 from machine import Pin, PWM, ADC, I2C
+from picozero import Button, Pot
 import onewire
 import ds18x20
 import ssd1306
 import math
 
-# servo motor
+# Power Button
+power_button = Button(2)
+
+# Setting Dials
+set_temp = Pot(27)
+set_od = Pot(28)
+
+
+# Read potentiometer as percent
+def pot_to_percent(pot):
+    return max(0, (pot.voltage - 0.23)) / 3.07
+
+
+# Servo stirring motor
 pwmservo = PWM(Pin(1))
 pwmservo.freq(50)
-# 1ms = round(65535/20)
-# 2ms = round(65535/10)
-# 1.5ms = round(65535*0.075)
 
 
-def servomove(power):
+def stirringPower(power):
     pwmservo.duty_u16(int((power/100)*1638.375+4915.125))
 
 
+# A convenience function for driving both the pump and heating coil
 def driveHBridge(speed, direction, speedGP, cwGP, acwGP):
     if speed > 100:
         speed = 100
@@ -37,51 +49,57 @@ def driveHBridge(speed, direction, speedGP, cwGP, acwGP):
         acw.value(0)
 
 
-# heating coil
+# Heating coil pins
 pwmheat = 2
 cwPinheat = 3
 acwPinheat = 4
 
 
+# Drive the heating coil
 def heatingPower(power):
     driveHBridge(power, 1, pwmheat, cwPinheat, acwPinheat)
 
 
-# pump , max speed = 60mL/min
-adcpump = ADC(Pin(27))
+# Pumping pins
 pwmpump = 13
 cwPinpump = 14
 acwPinpump = 15
 
 
+# Drive the pump
 def pumpPower(power):
     driveHBridge(power, 1, pwmpump, cwPinpump, acwPinpump)
 
 
-# temp sensor
+# Temperature scanning and selection
 ds_pin = Pin(21)
 ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
 rom = ds_sensor.scan()[0]
 
 
+# Read temperature in Celsius
 def readTemp():
     ds_sensor.convert_temp()
     return ds_sensor.read_temp(rom)
 
 
-# phototransistor
+# Phototransistor / OD pin
 photoPIN = 26
 
+
+# Read light intensity
 def readintensity():
     photoRes = ADC(Pin(photoPIN))
     intensity = photoRes.read_u16()
     return intensity
 
+
+# Convert two light readings to an OD
 def readOD(i1, i2):
-    od = -math.log10(i1/i2)
+    od = 2.28 * -math.log10(i1/i2)
     return od
 
 
-# oled display
+# OLED display pins
 i2c = I2C(0, sda=Pin(12), scl=Pin(9))
 display = ssd1306.SSD1306_I2C(128, 32, i2c)
--- Bioreactor/main.py
+++ Bioreactor/main.py
@@ -1,44 +1,72 @@
-from time import sleep, time_ns, time, ticks_us, ticks_diff
+from time import sleep, time, ticks_us, ticks_diff
 from drivers import *
 
+# Establish some global state
 stirringtimer = time()
 pidtimer = ticks_us()
 i1 = readintensity()
 esum = 0
+on = False
 
+# Code to run when the start / stop button is pressed
+def toggle_power():
+    global on, i1
+    on = not on
+    i1 = readintensity()
+    stirringPower(0)
+    pumpPower(0)
+
+
+# Trigger the `toggle_power()` function when the button is pressed
+power_button.when_pressed = toggle_power
+
+# Loop infinitely
 while True:
+    # Wait for a moment (200ms)
+    sleep(0.2)
+    # If the bioreactor is not powered on, skip to the next loop
+    if not on:
+        continue
+
+    # Set a fixed target temperature (will be user controlled in the future)
     target_temp = 37
+    # Establish some PI control parameters
     p = 10
     i = 0.1
     error = target_temp - readTemp()
-    pidtime = ticks_diff(ticks_us(),pidtimer)/1000000
+    # Scale error accumulation by elapsed time
+    pidtime = ticks_diff(ticks_us(), pidtimer) / 1000000
     esum += error * pidtime
-    print(error, esum)
     pidtimer = ticks_us()
-    heatingPower((p * error + i * esum) * 10)
+    # Set the heating power according to the PI controller's output
+    heatingPower(p * error + i * esum)
+
+    # Get the current time
     now = time()
-    if now-stirringtimer < 5:
-        servomove(-100) 
-    elif now-stirringtimer < 8:
-        servomove(0)
-    elif now-stirringtimer < 13:
-        servomove(100)
-    elif now-stirringtimer < 16:
-        servomove(0)
+    # Depending on how much time has passed, set the stirring direction power
+    if now - stirringtimer < 5:
+        stirringPower(-100)
+    elif now - stirringtimer < 8:
+        stirringPower(0)
+    elif now - stirringtimer < 13:
+        stirringPower(100)
+    elif now - stirringtimer < 16:
+        stirringPower(0)
     else:
+        # After getting this far, reset the timer to start over
         stirringtimer = time()
-        print(readTemp(), p + error + i * esum)
-#     print(now-stirringtimer)
+
+    # Take a new light reading
     i2 = readintensity()
+
+    # Fill the display with OD and temperature readings
     display.fill(0)
-    display.text("light: " + str(readOD(i1, i2)), 5, 5, 1)
-    display.text("temp : " + str(round(readTemp(), 1)) + "C", 5, 14, 1)
+    display.text(f"OD: {readOD(i1, i2):.3f}", 5, 5, 1)
+    display.text(f"Temp: {readTemp():.1f}C", 5, 14, 1)
     display.show()
 
+    # If the OD is greater than 0.5, dilute the media (will be user controlled)
     if readOD(i1, i2) > 0.5:
-        speed = adcpump.read_u16()
-        pumpPower(speed/65535*100)
-#         print(adcpump.read_u16())
+        pumpPower(100)
     else:
         pumpPower(0)
-    sleep(0.4) 
